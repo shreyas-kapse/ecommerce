14fce33bf6ce533e73c4b55536098660d2b7ac30
Shreyas <shreyaskapse171@gmail.com>
2024-08-03 15:38:02 +0530

    updated merchant service

--- src/main/java/com/e_commerce/backend/service/MerchantService.java
+++ src/main/java/com/e_commerce/backend/service/MerchantService.java
@@ -1,16 +1,35 @@
 package com.e_commerce.backend.service;
 
 import com.e_commerce.backend.DefaultResponse;
+import com.e_commerce.backend.Repository.MerchantRepository;
 import com.e_commerce.backend.enity.MerchantEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MerchantService implements IMerchantService {
+
+    @Autowired
+    private MerchantRepository merchantRepository;
+
     @Override
     public DefaultResponse addMerchant(MerchantEntity merchant) {
-         return DefaultResponse.builder()
+        if(merchantRepository.findByMerchantPhoneNo(merchant.getMerchantPhoneNo()).isPresent()){
+            return DefaultResponse.builder()
+                    .success(false)
+                    .message("Phone number already exists")
+                    .build();
+        }
+        if(merchantRepository.findByEmail(merchant.getEmail()).isPresent()){
+            return DefaultResponse.builder()
+                    .success(false)
+                    .message("Email already exists")
+                    .build();
+        }
+        merchantRepository.save(merchant);
+        return DefaultResponse.builder()
                 .success(true)
-                .message("Operation successful")
+                .message("Merchant added successfully")
                 .build();
     }
 }
