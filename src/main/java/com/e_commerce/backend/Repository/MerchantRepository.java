ac5bd6d7ecf8100164eae53a9a8cc7b6b1d29da7
Shreyas <shreyaskapse171@gmail.com>
2024-08-03 15:38:21 +0530

    created merchant repository

---
+++ src/main/java/com/e_commerce/backend/Repository/MerchantRepository.java
@@ -0,0 +1,13 @@
+package com.e_commerce.backend.Repository;
+
+import com.e_commerce.backend.enity.MerchantEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MerchantRepository extends JpaRepository<MerchantEntity,Long> {
+    Optional<MerchantEntity> findByMerchantPhoneNo(Long merchantPhoneNo);
+    Optional<MerchantEntity> findByEmail(String email);
+}
