3a393635d3b2ea6ba0741df5d57ff4bf17d8cd31
Shreyas <shreyaskapse171@gmail.com>
2024-08-15 12:24:09 +0530

    optimize jwt service

--- src/main/java/com/e_commerce/backend/service/JwtService.java
+++ src/main/java/com/e_commerce/backend/service/JwtService.java
@@ -1,5 +1,6 @@
 package com.e_commerce.backend.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -8,15 +9,12 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
-
-import io.jsonwebtoken.Claims;
 
 @Service
 public class JwtService {
