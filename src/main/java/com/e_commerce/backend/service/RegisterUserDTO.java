afaa5f34994da091869af197855b4d2d8498e7c9
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-02 00:14:55 +0530

    created register user DTO

---
+++ src/main/java/com/e_commerce/backend/service/RegisterUserDTO.java
@@ -0,0 +1,40 @@
+package com.e_commerce.backend.service;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class RegisterUserDTO {
+    @NotBlank(message = "user name is required")
+    private String username;
+
+    @NotBlank(message = "password is required")
+    private String password;
+
+    private String firstName;
+
+    private String lastName;
+
+    @NotBlank(message = "Phone number is required")
+    @Size(min = 10, max = 10, message = "Invalid number")
+    private String phoneNumber;
+
+    private String addressLine1;
+
+    private String addressLine2;
+
+    @NotBlank(message = "City name is required")
+    private String city;
+
+    @NotBlank(message = "State name is required")
+    private String state;
+
+    @NotBlank(message = "Postal code is required")
+    private String postalCode;
+
+    @NotBlank(message = "Country name is required")
+    private String country;
+}
