4d4d354f622c79d2e7cba3f2d268772808e09fe5
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-02 00:15:29 +0530

    added method to find user by username

--- src/main/java/com/e_commerce/backend/Repository/UserRepository.java
+++ src/main/java/com/e_commerce/backend/Repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.e_commerce.backend.enity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity,Long> {
 
     UserEntity findByUsername(String username);
+
+    Optional<UserEntity> findUserByUsername(String userName);
 }
