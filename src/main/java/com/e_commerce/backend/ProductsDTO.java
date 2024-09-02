fc5838e118ecc873b81834ca8f384d8037a1bf44
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-02 22:41:11 +0530

    created DTO to return the list of products

---
+++ src/main/java/com/e_commerce/backend/ProductsDTO.java
@@ -0,0 +1,22 @@
+package com.e_commerce.backend;
+
+import com.e_commerce.backend.enity.ProductEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ProductsDTO {
+    private DefaultResponse response;
+    private Optional<Integer> totalProducts;
+    private Optional<List<ProductEntity>> products;
+}
