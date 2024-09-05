46cfddc007743f3273ef93d72dd1291502739a86
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-05 15:31:27 +0530

    created API to add product in cart

---
+++ src/main/java/com/e_commerce/backend/controller/CartController.java
@@ -0,0 +1,35 @@
+package com.e_commerce.backend.controller;
+
+import com.e_commerce.backend.DefaultResponse;
+import com.e_commerce.backend.service.ICartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+
+    @Autowired
+    private ICartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<DefaultResponse> addProductToCart(@RequestHeader("Authorization") String authorizationHeader, @RequestParam Long productId, @RequestParam int quantity) {
+        String token = "";
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            token = authorizationHeader.substring(7);
+        }
+        if (token.isEmpty()) {
+            DefaultResponse errorResponse = DefaultResponse.builder()
+                    .success(false)
+                    .message("Error in session please login")
+                    .build();
+            return ResponseEntity.internalServerError().body(errorResponse);
+        }
+        DefaultResponse response = cartService.addProductToCart(productId, quantity, token);
+        if (!response.isSuccess()) {
+            return ResponseEntity.status(response.getHttpStatus().get()).body(response);
+        }
+        return ResponseEntity.status(response.getHttpStatus().get()).body(response);
+    }
+}
