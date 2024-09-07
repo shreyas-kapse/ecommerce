c53309a4484b3c2924ff81623e27d50e03ab48d0
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-08 00:08:59 +0530

    created service to send mail

---
+++ src/main/java/com/e_commerce/backend/EmailService.java
@@ -0,0 +1,26 @@
+package com.e_commerce.backend;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendMail(String to, String subject, String body, String from){
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(to);
+        simpleMailMessage.setSubject(subject);
+        simpleMailMessage.setText(body);
+
+        javaMailSender.send(simpleMailMessage);
+    }
+}
