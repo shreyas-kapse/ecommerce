8d04ec2b8740ad5f3a0197efea86d30db4b6a614
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-08 00:06:36 +0530

    created DTO to return cart

---
+++ src/main/java/com/e_commerce/backend/CartDTOResponse.java
@@ -0,0 +1,20 @@
+package com.e_commerce.backend;
+
+import com.e_commerce.backend.enity.CartEntity;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class CartDTOResponse {
+    private DefaultResponse response;
+    private Optional<CartEntity> cart;
+}
