12a56bd791fa3a0aedaa7db371c88c18dc8ddd85
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-10 18:02:42 +0530

    updated method to send mail on sign up with login credentials

--- src/main/java/com/e_commerce/backend/service/MerchantService.java
+++ src/main/java/com/e_commerce/backend/service/MerchantService.java
@@ -2,11 +2,13 @@ package com.e_commerce.backend.service;
 
 import com.e_commerce.backend.AccountStatus;
 import com.e_commerce.backend.DefaultResponse;
+import com.e_commerce.backend.EmailService;
 import com.e_commerce.backend.Repository.MerchantRepository;
 import com.e_commerce.backend.Repository.UserRepository;
 import com.e_commerce.backend.Role;
 import com.e_commerce.backend.enity.MerchantEntity;
 import com.e_commerce.backend.enity.UserEntity;
+import com.e_commerce.backend.utils.PasswordGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,15 +24,21 @@ public class MerchantService implements IMerchantService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordGenerator passwordGenerator;
+
+    @Autowired
+    private EmailService emailService;
+
     @Override
     public DefaultResponse addMerchant(MerchantEntity merchant) {
-        if(merchantRepository.findByMerchantPhoneNo(merchant.getMerchantPhoneNo()).isPresent()){
+        if (merchantRepository.findByMerchantPhoneNo(merchant.getMerchantPhoneNo()).isPresent()) {
             return DefaultResponse.builder()
                     .success(false)
                     .message("Phone number already exists")
                     .build();
         }
-        if(merchantRepository.findByEmail(merchant.getEmail()).isPresent()){
+        if (merchantRepository.findByEmail(merchant.getEmail()).isPresent()) {
             return DefaultResponse.builder()
                     .success(false)
                     .message("Email already exists")
@@ -38,6 +46,9 @@ public class MerchantService implements IMerchantService {
         }
         merchant.setAccountStatus(AccountStatus.PENDING.name());
         merchantRepository.save(merchant);
+
+        String password = passwordGenerator.generatePassword(12);
+        emailService.merchantAccountCreationMail(merchant.getEmail(), "Your Merchant Account is Ready - Login Details Enclosed", "admin@ecommerce.com", merchant.getFirstName() + " " + merchant.getLastName(), merchant.getEmail(), password);
 
         UserEntity user = UserEntity.builder()
                 .username(merchant.getEmail())
@@ -50,7 +61,7 @@ public class MerchantService implements IMerchantService {
                 .phoneNumber(merchant.getMerchantPhoneNo().toString())
                 .isEmailVerified(false)
                 .postalCode(merchant.getPinCode().toString())
-                .password(new BCryptPasswordEncoder(12).encode("abc"))
+                .password(new BCryptPasswordEncoder(12).encode(password))
                 .accountStatus(AccountStatus.PENDING.name())
                 .role(Role.MERCHANT.name())
                 .build();
@@ -58,14 +69,14 @@ public class MerchantService implements IMerchantService {
 
         return DefaultResponse.builder()
                 .success(true)
-                .message("Merchant added successfully")
+                .message("Merchant added successfully, Login credentials have been sent via email.")
                 .build();
     }
 
     @Override
     public Object getMerchant(String email, String companyName) {
         Optional<MerchantEntity> merchant = merchantRepository.findByEmail(email);
-        if(merchant.isPresent() && merchant.get().getCompanyName().equalsIgnoreCase(companyName)){
+        if (merchant.isPresent() && merchant.get().getCompanyName().equalsIgnoreCase(companyName)) {
             return merchant;
         }
         return DefaultResponse.builder().message("No merchant found").build();
