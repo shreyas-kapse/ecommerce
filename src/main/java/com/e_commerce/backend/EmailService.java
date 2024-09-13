565f40d7341a9e9184758a6259be454eb6577238
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-13 13:30:30 +0530

    created method to send mail for order confirmation

--- src/main/java/com/e_commerce/backend/EmailService.java
+++ src/main/java/com/e_commerce/backend/EmailService.java
@@ -1,5 +1,6 @@
 package com.e_commerce.backend;
 
+import com.e_commerce.backend.enity.OrderItem;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -10,6 +11,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 public class EmailService {
@@ -49,6 +51,39 @@ public class EmailService {
             String htmlContent = htmlTemplate.replace("${merchantName}", name)
                     .replace("${username}", username)
                     .replace("${password}", password);
+            message.setContent(htmlContent, "text/html; charset=utf-8");
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            System.out.println("Exception occurred while sending mail.");
+        }
+    }
+
+    @Async
+    public void orderConfirmationMail(String to, String subject, String from, String customerName, String orderNumber, String orderDate, List<OrderItem> orderItems, String shippingAddress, String deliveryDate) {
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+
+            message.setRecipients(MimeMessage.RecipientType.TO, to);
+            message.setFrom(from);
+            message.setSubject(subject);
+            String htmlTemplate = readFile("src/main/resources/mailTemplate/OrderConfirmation.html");
+
+            StringBuilder orderedItemsHtml = new StringBuilder();
+
+            for (OrderItem item : orderItems) {
+                orderedItemsHtml.append("<tr>")
+                        .append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(item.getProduct().getProductName()).append("</td>")
+                        .append("<td style='border: 1px solid #ddd; padding: 8px;'>").append(item.getQuantity()).append("</td>")
+                        .append("<td style='border: 1px solid #ddd; padding: 8px;'>$").append(item.getPrice()).append("</td>")
+                        .append("</tr>");
+            }
+            String htmlContent = htmlTemplate.replace("${customerName}", customerName)
+                    .replace("${orderNumber}", orderNumber)
+                    .replace("${orderDate}", orderDate)
+                    .replace("${orderedItems}", orderedItemsHtml.toString())
+                    .replace("${shippingAddress}",shippingAddress)
+                    .replace("${deliveryDate}",deliveryDate);
+
             message.setContent(htmlContent, "text/html; charset=utf-8");
             javaMailSender.send(message);
         } catch (Exception e) {
