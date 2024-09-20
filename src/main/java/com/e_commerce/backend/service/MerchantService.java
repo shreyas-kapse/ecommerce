0933aa2a8ef84138fb98bae0222670348b0fb9fc
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-20 23:44:34 +0530

    handled exceptions and added logger

--- src/main/java/com/e_commerce/backend/service/MerchantService.java
+++ src/main/java/com/e_commerce/backend/service/MerchantService.java
@@ -6,15 +6,19 @@ import com.e_commerce.backend.EmailService;
 import com.e_commerce.backend.Repository.MerchantRepository;
 import com.e_commerce.backend.Repository.UserRepository;
 import com.e_commerce.backend.Role;
+import com.e_commerce.backend.dtos.MerchantDTO;
 import com.e_commerce.backend.enity.MerchantEntity;
 import com.e_commerce.backend.enity.UserEntity;
 import com.e_commerce.backend.utils.PasswordGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class MerchantService implements IMerchantService {
 
@@ -32,53 +36,96 @@ public class MerchantService implements IMerchantService {
 
     @Override
     public DefaultResponse addMerchant(MerchantEntity merchant) {
-        if (merchantRepository.findByMerchantPhoneNo(merchant.getMerchantPhoneNo()).isPresent()) {
+        try {
+            String merchantName = merchant.getFirstName() + " " + merchant.getLastName();
+            log.info("Processing add merchant request for merhchant with name {} and company name {}", merchantName, merchant.getCompanyName());
+            if (merchantRepository.findByMerchantPhoneNo(merchant.getMerchantPhoneNo()).isPresent()) {
+                return DefaultResponse.builder()
+                        .success(false)
+                        .message("Phone number already exists")
+                        .build();
+            }
+            if (merchantRepository.findByEmail(merchant.getEmail()).isPresent()) {
+                return DefaultResponse.builder()
+                        .success(false)
+                        .message("Email already exists")
+                        .build();
+            }
+            merchant.setAccountStatus(AccountStatus.PENDING.name());
+            merchantRepository.save(merchant);
+
+            String password = passwordGenerator.generatePassword(12);
+            emailService.merchantAccountCreationMail(merchant.getEmail(),
+                    "Your Merchant Account is Ready - Login Details Enclosed",
+                    "admin@ecommerce.com",
+                    merchant.getFirstName() + " " + merchant.getLastName(),
+                    merchant.getEmail(),
+                    password);
+
+            UserEntity user = UserEntity.builder()
+                    .username(merchant.getEmail())
+                    .firstName(merchant.getFirstName())
+                    .lastName(merchant.getLastName())
+                    .addressLine1(merchant.getOfficeAddress())
+                    .city(merchant.getCity())
+                    .country(merchant.getCountry())
+                    .state(merchant.getState())
+                    .phoneNumber(merchant.getMerchantPhoneNo().toString())
+                    .isEmailVerified(false)
+                    .postalCode(merchant.getPinCode().toString())
+                    .password(new BCryptPasswordEncoder(12).encode(password))
+                    .accountStatus(AccountStatus.PENDING.name())
+                    .role(Role.MERCHANT.name())
+                    .build();
+            userRepository.save(user);
+
+            log.info("Successfully processed add merchant request for merchant with name {}", merchantName);
+
+            return DefaultResponse.builder()
+                    .success(true)
+                    .message("Merchant added successfully, Login credentials have been sent via email.")
+                    .build();
+        } catch (Exception exception) {
+            log.error("Error occurred while processing add merchant request with error {} ", exception.getMessage());
             return DefaultResponse.builder()
                     .success(false)
-                    .message("Phone number already exists")
+                    .message("Error occurred while processing add merchant request")
+                    .httpStatus(Optional.of(HttpStatus.INTERNAL_SERVER_ERROR))
                     .build();
         }
-        if (merchantRepository.findByEmail(merchant.getEmail()).isPresent()) {
-            return DefaultResponse.builder()
-                    .success(false)
-                    .message("Email already exists")
-                    .build();
-        }
-        merchant.setAccountStatus(AccountStatus.PENDING.name());
-        merchantRepository.save(merchant);
-
-        String password = passwordGenerator.generatePassword(12);
-        emailService.merchantAccountCreationMail(merchant.getEmail(), "Your Merchant Account is Ready - Login Details Enclosed", "admin@ecommerce.com", merchant.getFirstName() + " " + merchant.getLastName(), merchant.getEmail(), password);
-
-        UserEntity user = UserEntity.builder()
-                .username(merchant.getEmail())
-                .firstName(merchant.getFirstName())
-                .lastName(merchant.getLastName())
-                .addressLine1(merchant.getOfficeAddress())
-                .city(merchant.getCity())
-                .country(merchant.getCountry())
-                .state(merchant.getState())
-                .phoneNumber(merchant.getMerchantPhoneNo().toString())
-                .isEmailVerified(false)
-                .postalCode(merchant.getPinCode().toString())
-                .password(new BCryptPasswordEncoder(12).encode(password))
-                .accountStatus(AccountStatus.PENDING.name())
-                .role(Role.MERCHANT.name())
-                .build();
-        userRepository.save(user);
-
-        return DefaultResponse.builder()
-                .success(true)
-                .message("Merchant added successfully, Login credentials have been sent via email.")
-                .build();
     }
 
     @Override
-    public Object getMerchant(String companyName) {
-        Optional<MerchantEntity> merchant = merchantRepository.findByCompanyName(companyName);
-        if (merchant.isPresent() && merchant.get().getCompanyName().equalsIgnoreCase(companyName)) {
-            return merchant;
+    public MerchantDTO getMerchant(String companyName) {
+        try {
+            log.info("Processing get merchant request for company name {}", companyName);
+            Optional<MerchantEntity> merchant = merchantRepository.findByCompanyName(companyName);
+
+            if (merchant.isPresent() && merchant.get().getCompanyName().equalsIgnoreCase(companyName)) {
+                log.info("Successfully processed get merchant request for merchant with company name {}", companyName);
+                return MerchantDTO.builder()
+                        .response(DefaultResponse.builder()
+                                .success(true)
+                                .build())
+                        .merchant(merchant)
+                        .build();
+            }
+            return MerchantDTO.builder()
+                    .response(DefaultResponse.builder()
+                            .success(false)
+                            .message("No merchant found")
+                            .httpStatus(Optional.of(HttpStatus.NO_CONTENT))
+                            .build())
+                    .build();
+        } catch (Exception exception) {
+            log.error("Error occurred while processing get merchant request with error {} ", exception.getMessage());
+            return MerchantDTO.builder()
+                    .response(DefaultResponse.builder()
+                            .success(false)
+                            .message("Error occurred while fetching merchant")
+                            .httpStatus(Optional.of(HttpStatus.INTERNAL_SERVER_ERROR))
+                            .build())
+                    .build();
         }
-        return DefaultResponse.builder().message("No merchant found").build();
     }
 }
