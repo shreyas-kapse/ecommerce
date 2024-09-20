49b6989af5e5ac515cd0da2fde71d963fbaf69d0
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-20 23:44:49 +0530

    updated method return type

--- src/main/java/com/e_commerce/backend/service/IMerchantService.java
+++ src/main/java/com/e_commerce/backend/service/IMerchantService.java
@@ -1,9 +1,10 @@
 package com.e_commerce.backend.service;
 
 import com.e_commerce.backend.DefaultResponse;
+import com.e_commerce.backend.dtos.MerchantDTO;
 import com.e_commerce.backend.enity.MerchantEntity;
 
 public interface IMerchantService {
     public DefaultResponse addMerchant(MerchantEntity merchant);
-    public Object getMerchant(String companyName);
+    public MerchantDTO getMerchant(String companyName);
 }
