b4ea6775564211ce721e2ec4da44b67819d594d3
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-21 00:04:35 +0530

    added logger and updated controller

--- src/main/java/com/e_commerce/backend/controller/ProductController.java
+++ src/main/java/com/e_commerce/backend/controller/ProductController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
@@ -17,6 +18,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+@Slf4j
 @Tag(name = "Product ", description = "Operations related to the product")
 @RestController
 @RequestMapping("/product")
@@ -52,7 +54,7 @@ public class ProductController {
                     .build();
             return ResponseEntity.internalServerError().body(response);
         }
-
+        log.info("Processing add product request for product with name {} and brand name {}", productEntity.getProductName(), productEntity.getBrand());
         response = productService.addProduct(productEntity, token);
 
         return !response.isSuccess() ? ResponseEntity.status(response.getHttpStatus().get()).body(response) : ResponseEntity.ok(response);
@@ -61,8 +63,9 @@ public class ProductController {
     @GetMapping("/merchant/{merchantName}")
     @Operation(summary = "Get merchant by merhcant name")
     public ResponseEntity<ProductsDTO> getProductsByMerchantName(
-            @Parameter(description = "Merchant name") @PathVariable String merchantName
+            @Parameter(description = "Merchant Name") @PathVariable("merchantName") String merchantName
     ) {
+        log.info("Processing get merchant by merchant name request for the merchant with name {}", merchantName);
         ProductsDTO productsDTO = productService.getProductsByMerchantName(merchantName);
         if (!productsDTO.getResponse().isSuccess()) {
             return ResponseEntity.status(productsDTO.getResponse().getHttpStatus().get()).body(productsDTO);
@@ -73,8 +76,9 @@ public class ProductController {
     @GetMapping("/brand/{brand}")
     @Operation(summary = "Get products by brand name", description = "List all products of given brand")
     public ResponseEntity<ProductsDTO> getProductsByBrandName(
-            @Parameter(description = "Brand name") @PathVariable String brand
+            @Parameter(description = "Brand name") @PathVariable("brand") String brand
     ) {
+        log.info("Processing get products by brand name request for {} brand", brand);
         ProductsDTO productsDTO = productService.getProductsByBrandName(brand);
         if (!productsDTO.getResponse().isSuccess()) {
             return ResponseEntity.status(productsDTO.getResponse().getHttpStatus().get()).body(productsDTO);
@@ -85,8 +89,9 @@ public class ProductController {
     @GetMapping("/category/{categoryName}")
     @Operation(summary = "Get products by category name", description = "List all products of given category")
     public ResponseEntity<ProductsDTO> getProductsByCategoryName(
-            @Parameter(description ="Category name")@PathVariable String categoryName
+            @Parameter(description ="Category Name") @PathVariable("categoryName") String categoryName
     ) {
+        log.info("Processing get products by category name request for {} category", categoryName);
         ProductsDTO productsDTO = productService.getProductsByCategoryName(categoryName);
         if (!productsDTO.getResponse().isSuccess()) {
             return ResponseEntity.status(productsDTO.getResponse().getHttpStatus().get()).body(productsDTO);
@@ -97,8 +102,9 @@ public class ProductController {
     @GetMapping("/{id}")
     @Operation(summary = "Get product by id")
     public ResponseEntity<ProductsDTO> getProductsById(
-            @Parameter(description = "Product id") @PathVariable String id
+            @Parameter(description = "Product Id") @PathVariable("id") String id
     ) {
+        log.info("Processing get product by product id {}", id);
         ProductsDTO productsDTO = productService.getProductById(id);
         if (!productsDTO.getResponse().isSuccess()) {
             return ResponseEntity.status(productsDTO.getResponse().getHttpStatus().get()).body(productsDTO);
