1428a1a780a8232b34a2db6fcc9f5af7e31bda2c
Shreyas Kapse <shreyaskapse171@gmail.com>
2024-09-21 00:34:27 +0530

    handled exceptions and added logger

--- src/main/java/com/e_commerce/backend/service/ProductService.java
+++ src/main/java/com/e_commerce/backend/service/ProductService.java
@@ -6,6 +6,7 @@ import com.e_commerce.backend.Repository.MerchantRepository;
 import com.e_commerce.backend.Repository.ProductRepository;
 import com.e_commerce.backend.enity.MerchantEntity;
 import com.e_commerce.backend.enity.ProductEntity;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class ProductService implements IProductService {
 
@@ -29,6 +31,7 @@ public class ProductService implements IProductService {
     public DefaultResponse addProduct(ProductEntity productEntity, String token) {
         try {
             String email = jwtService.extractUserName(token);
+
             Optional<MerchantEntity> merchant = merchantRepository.findByEmail(email);
             if (merchant.isEmpty()) {
                 return DefaultResponse.builder()
@@ -38,13 +41,18 @@ public class ProductService implements IProductService {
                         .build();
             }
             MerchantEntity merchantEntity = merchant.get();
+            log.info("Processing add product request for merchant with id {}", merchantEntity.getId());
+
             productEntity.setMerchant(merchantEntity);
             productRepository.save(productEntity);
+
+            log.info("Successfully processed add product request for merchant with id {}", merchantEntity.getId());
             return DefaultResponse.builder()
                     .success(true)
                     .message("Product added successfully")
                     .build();
         } catch (Exception exception) {
+            log.error("Error occurred while processing add product request with error {} ", exception.getMessage());
             return DefaultResponse.builder()
                     .success(false)
                     .message("Error occurred while adding new product")
@@ -55,39 +63,56 @@ public class ProductService implements IProductService {
 
     @Override
     public ProductsDTO getProductsByMerchantName(String merchantName) {
-        Optional<MerchantEntity> merchant = merchantRepository.findByCompanyName(merchantName);
-        if (merchant.isEmpty()) {
+        try {
+            Optional<MerchantEntity> merchant = merchantRepository.findByCompanyName(merchantName);
+            if (merchant.isEmpty()) {
+                return ProductsDTO.builder()
+                        .response(DefaultResponse.builder()
+                                .success(false)
+                                .message("No company found")
+                                .httpStatus(Optional.of(HttpStatus.NO_CONTENT))
+                                .build())
+                        .build();
+            }
+            log.info("Processing get products by merchant name request for merchant with merchant name {}", merchantName);
+
+            Optional<List<ProductEntity>> products = productRepository.findAllByMerchantId(merchant.get().getId());
+            if (products.isEmpty()) {
+                return ProductsDTO.builder()
+                        .response(DefaultResponse.builder()
+                                .success(false)
+                                .httpStatus(Optional.of(HttpStatus.NO_CONTENT))
+                                .message("No products found")
+                                .build())
+                        .build();
+            }
+
+            log.info("Successfully processed get products by merchant name request for merchant with merchant name {}", merchantName);
+            return ProductsDTO.builder()
+                    .response(DefaultResponse.builder()
+                            .success(true)
+                            .build())
+                    .products(Optional.of(products.get()))
+                    .totalProducts(Optional.of(products.get().size()))
+                    .build();
+        } catch (Exception e) {
+            log.error("Error occurred while processing get products by merchant name request with merchant name {} and error {}", merchantName, e.getMessage());
             return ProductsDTO.builder()
                     .response(DefaultResponse.builder()
                             .success(false)
-                            .message("No company found")
-                            .httpStatus(Optional.of(HttpStatus.NO_CONTENT))
+                            .httpStatus(Optional.of(HttpStatus.INTERNAL_SERVER_ERROR))
+                            .message("Error occurred while processing get products by merchant name")
                             .build())
                     .build();
         }
-        Optional<List<ProductEntity>> products = productRepository.findAllByMerchantId(merchant.get().getId());
-        if (products.isEmpty()) {
-            return ProductsDTO.builder()
-                    .response(DefaultResponse.builder()
-                            .success(false)
-                            .httpStatus(Optional.of(HttpStatus.NO_CONTENT))
-                            .message("No products found")
-                            .build())
-                    .build();
-        }
-        return ProductsDTO.builder()
-                .response(DefaultResponse.builder()
-                        .success(true)
-                        .build())
-                .products(Optional.of(products.get()))
-                .totalProducts(Optional.of(products.get().size()))
-                .build();
     }
 
     @Override
     public ProductsDTO getProductsByBrandName(String brandName) {
         try {
             Optional<List<ProductEntity>> products = productRepository.findAllByBrand(brandName);
+            log.info("Processing get products by brand name request for brand name {}", brandName);
+
             if (products.get().isEmpty()) {
                 return ProductsDTO.builder()
                         .response(DefaultResponse.builder()
@@ -97,6 +122,8 @@ public class ProductService implements IProductService {
                                 .build())
                         .build();
             }
+            log.info("Successfully processed get products by brand name request for brand name {}", brandName);
+
             return ProductsDTO.builder()
                     .totalProducts(Optional.of(products.get().size()))
                     .products(Optional.of(products.get()))
@@ -106,6 +133,7 @@ public class ProductService implements IProductService {
                             .build())
                     .build();
         } catch (Exception e) {
+            log.error("Error occurred while processing get products by brand name request with brand name {} and error {}", brandName, e.getMessage());
             return ProductsDTO.builder()
                     .response(DefaultResponse.builder()
                             .success(false)
@@ -120,6 +148,8 @@ public class ProductService implements IProductService {
     public ProductsDTO getProductsByCategoryName(String categoryName) {
         try {
             Optional<List<ProductEntity>> products = productRepository.findAllByCategory(categoryName);
+            log.info("Processing get products by category name request for category name {}", categoryName);
+
             if (products.get().isEmpty()) {
                 return ProductsDTO.builder()
                         .response(DefaultResponse.builder()
@@ -129,6 +159,8 @@ public class ProductService implements IProductService {
                                 .build())
                         .build();
             }
+
+            log.info("Successfully processed get products by category name request for category name {}", categoryName);
             return ProductsDTO.builder()
                     .totalProducts(Optional.of(products.get().size()))
                     .products(Optional.of(products.get()))
@@ -138,6 +170,7 @@ public class ProductService implements IProductService {
                             .build())
                     .build();
         } catch (Exception e) {
+            log.error("Error occurred while processing get products by category name request with category name {} and error {}", categoryName, e.getMessage());
             return ProductsDTO.builder()
                     .response(DefaultResponse.builder()
                             .success(false)
@@ -152,6 +185,8 @@ public class ProductService implements IProductService {
     public ProductsDTO getProductById(String id) {
         try {
             Optional<ProductEntity> product = productRepository.findById(Long.valueOf(id));
+            log.info("Processing get product by id request with id {}", id);
+
             if (product.isEmpty()) {
                 return ProductsDTO.builder()
                         .response(DefaultResponse.builder()
@@ -161,6 +196,8 @@ public class ProductService implements IProductService {
                                 .build())
                         .build();
             }
+
+            log.info("Successfully processed get product by id request with id {}", id);
             return ProductsDTO.builder()
                     .products(Optional.of(List.of(product.get())))
                     .response(DefaultResponse.builder()
@@ -169,6 +206,7 @@ public class ProductService implements IProductService {
                             .build())
                     .build();
         } catch (Exception e) {
+            log.error("Error occurred while processing get product by id with id {} and error {}", id, e.getMessage());
             return ProductsDTO.builder()
                     .response(DefaultResponse.builder()
                             .success(false)
